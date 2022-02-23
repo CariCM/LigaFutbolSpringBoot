82dbf01eca963b178653f6b91464ac568470e225
Carina M <kry_mira_2017@yahoo.com>
2022-02-22 23:46:33 -0300

    Agrege 2 paquetes repository y service, con 2 clases en el repository.

---
+++ src/main/java/ar/edu/unju/fi/repository/EquipoDaoImp.java
@@ -0,0 +1,5 @@
+package ar.edu.unju.fi.repository;
+//eqipodaoimp implemente nuestra interface
+public class EquipoDaoImp  implements IEquipoDAO {
+
+}
---
+++ src/main/java/ar/edu/unju/fi/repository/IEquipoDAO.java
@@ -0,0 +1,5 @@
+package ar.edu.unju.fi.repository;
+//daO PATRON DE DISEÑO, CREADO PARA FACILITAR EL TRABAJO CON ACCESO A DATOS.
+public interface IEquipoDAO {
+
+}
